719f502278e14ba796f535abb9dacdfaaf641882
apasarkar <amolpasarkar@gmail.com>
2025-03-19 00:54:58 +0800

    Includes complete rigid registration pipeline

--- masknmf/motion_correction/registration_methods.py
+++ masknmf/motion_correction/registration_methods.py
@@ -47,6 +47,61 @@ def apply_rigid_shifts(imgs: torch.tensor,
     return torch.real(shifted_imgs)
 
 
+def estimate_rigid_shifts(image_stack: torch.tensor,
+                          template: torch.tensor,
+                          max_shifts: tuple[int, int]) -> torch.tensor:
+    """
+    Estimate rigid shifts to apply to a given image stack to best align each frame to a template
+
+    Args:
+        image_stack (torch.tensor): Shape (num_frames, fov dim1, fov dim2).
+        template (torch.tensor): Shape (fov dim1, fov dim2).
+        max_shifts (tuple[int, int]): Maximum shifts we can apply in each direction
+    Returns:
+        corrected_stack (torch.tensor): Shape (num_frames, fov dim1, fov dim2)
+    """
+
+    num_frames, d1, d2 = image_stack.shape
+    device = image_stack.device
+    fft_image_stack = torch.fft.fft2(image_stack)
+    fft_template = torch.conj(torch.fft.fft2(template))
+    max_shifts = torch.tensor(max_shifts).to(device)
+
+    fft_cross_correlation = fft_image_stack * fft_template[None, :, :]
+    spatial_domain_cross_correlation = torch.real(torch.fft.ifft2(fft_cross_correlation, norm="backward"))
+
+    dim1_valid_shifts = torch.arange(d1, device=device)
+    dim1_valid_locations = torch.logical_or(dim1_valid_shifts >= d1 - 1 - torch.abs(max_shifts[0]),
+                                            dim1_valid_shifts <= torch.abs(max_shifts[0])).float()
+
+    dim2_valid_shifts = torch.arange(d2, device=device)
+    dim2_valid_locations = torch.logical_or(dim2_valid_shifts >= d2 - 1 - torch.abs(max_shifts[1]),
+                                            dim2_valid_shifts <= torch.abs(max_shifts[1])).float()
+
+    valid_locations = dim1_valid_locations[:, None] @ dim2_valid_locations[None, :]
+    invalid_locations = (~(valid_locations.bool())).float()
+    invalid_subtraction = invalid_locations * torch.abs(torch.amax(spatial_domain_cross_correlation))
+    cross_correlation_values = spatial_domain_cross_correlation * valid_locations[None, :, :].float()
+
+    # Guarantees that the max cross correlation happens at |shift value| < |max shifts| in both dimensions
+    cross_correlation_values -= invalid_subtraction
+
+    max_indices = torch.argmax(cross_correlation_values.reshape((num_frames, -1)), dim=1)
+    shifts_dim1, shifts_dim2 = torch.unravel_index(max_indices, (d1, d2))
+
+    values_to_subtract_dim1 = (torch.abs(d1 - shifts_dim1) < torch.abs(shifts_dim1)).long()
+    shifts_dim1 -= values_to_subtract_dim1 * (d1 - 1)
+
+    values_to_subtract_dim2 = (torch.abs(d2 - shifts_dim2) < torch.abs(shifts_dim2)).long()
+    shifts_dim2 -= values_to_subtract_dim2 * (d2 - 1)
+
+    shifts = torch.stack([shifts_dim1, shifts_dim2], dim=1)
+
+    # These shifts keep the image fixed and find the optimal template shift; we want the opposite (shift image --> match to template)
+    shifts *= -1
+
+    return shifts
+
 def _interpolate_to_border(shifted_imgs: torch.tensor,
                            shifts: torch.tensor):
     """
