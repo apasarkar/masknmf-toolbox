5fe2d7955581ff0ecee105c8d3154ce687cfe84c
apasarkar <amolpasarkar@gmail.com>
2025-05-01 06:34:12 +0800

    Includes new residual pmd array object for quick inspection of residual results

--- masknmf/compression/__init__.py
+++ masknmf/compression/__init__.py
@@ -1,10 +1,11 @@
 from .decomposition import compute_lowrank_factorized_svd, pmd_decomposition, pmd_batch
-from .pmd_array import PMDArray, convert_dense_image_stack_to_pmd_format
+from .pmd_array import PMDArray, PMDResidualArray, convert_dense_image_stack_to_pmd_format
 
 __all__ = [
     "compute_lowrank_factorized_svd",
     "pmd_decomposition",
     "pmd_batch",
     "PMDArray",
+    "PMDResidualArray",
     "convert_dense_image_stack_to_pmd_format",
 ]
