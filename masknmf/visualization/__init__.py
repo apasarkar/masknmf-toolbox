9ffd3f38301c837d7de5655119d03d7bd7de207e
Amol Pasarkar <amolpasarkar@gmail.com>
2025-05-12 23:46:34 -0400

    Includes PMD vs Raw stack diagnostic plotting code (#13)

--- masknmf/visualization/__init__.py
+++ masknmf/visualization/__init__.py
@@ -4,4 +4,10 @@ from .interactive_guis import (
     get_correlation_widget,
     make_demixing_video,
 )
-from .plots import construct_index, plot_ith_roi
+from .plots import (
+    construct_index,
+    plot_ith_roi,
+    plot_pmd_vs_raw_stack_diagnostic,
+    generate_raw_vs_resid_plot_folder,
+    roi_compare_pmd_raw,
+)
