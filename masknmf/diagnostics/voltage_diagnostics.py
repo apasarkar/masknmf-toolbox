c7da0cf2424c663c2082f3ed9021f8c9ccedcde5
apasarkar <amolpasarkar@gmail.com>
2025-05-24 16:25:20 +0800

    Includes new code for onep voltage culture analysis

--- masknmf/diagnostics/voltage_diagnostics.py
+++ masknmf/diagnostics/voltage_diagnostics.py
@@ -48,3 +48,83 @@ def detect_peaks(trace, height=None, distance=None, prominence=None):
     """
     peaks, properties = find_peaks(trace, height=height, distance=distance, prominence=prominence)
     return peaks, properties
+
+
+def onephoton_voltage_snr_stats(full_moco_dense: np.ndarray,
+                                pmd_arr_moco: masknmf.PMDArray,
+                                spatial_footprint: np.ndarray,
+                                temporal_trace: np.ndarray,
+                                trend_filter_cutoff_freq: float = 1,
+                                spike_detect_cutoff_freq: float = 3,
+                                mad_cutoff=8,
+                                sampling_rate: float = 800,
+                                reverse_polarity: bool = True):
+    """
+    General pipeline for studying the spikes present in the same ROI average across two image stacks.
+    Assumption: the pmd array (pmd_arr_moco) has been "flipped" if the indicator is negative (i.e. all spikes point upwards in that stack).
+    Args:
+        full_moco_dense: dense stack (frames, fov_dim1, fov_dim2)
+        pmd_arr_moco: masknmf.PMDArray object
+        spatial_footprint (np.ndarray). Shape (fov_dim1, fov_dim2)
+        temporal_trace (np.ndarray): Shape (num_frames,)
+        trend_filter_cutoff_freq (float): the frequency cutoff for highpass filtering to remove optostim related smooth trends
+        spike_detect_cutoff_freq (float): the highpass filter frequency used before running MAD thresholding to identify spikes
+        mad_cutoff (float): the mad threshold used to identify spiking locations (and eventually find the spike peak times).
+        sampling_rate (float): The sampling rate of the data in Hz
+        reverse_polarity (bool): True if the raw stack contains a negatively tuned indicator.
+
+    Returns:
+        - np.ndarray: raw roi average time series
+        - np.ndarray: raw pmd roi average time series
+        - np.ndarray: time series indicating estimated spike peak frame locations
+        - np.ndarray: time series indicating pmd spike height estimates
+        - np.ndarray: time series indicating raw data spike height estimates
+        - np.ndarray: time series indicating the spike height 'attenuation' estimate
+        - np.ndarray: time series indicating the spike height attenuation as fraction of raw spike height (we disregard negative values here)
+        - np.ndarray: the spike height attenuation divided by the residual standard deviation
+    """
+
+    raw_roi_avg, pmd_roi_avg = masknmf.visualization.plots.roi_compare_pmd_raw(full_moco_dense,
+                                                                               pmd_arr_moco,
+                                                                               spatial_footprint)
+
+    if reverse_polarity:
+        raw_roi_avg *= -1
+        raw_roi_avg -= np.mean(raw_roi_avg)
+
+    pmd_roi_avg -= np.mean(pmd_roi_avg)
+
+    pmd_roi_lowpass = pmd_roi_avg - masknmf.demixing.filters.high_pass_filter_batch(pmd_roi_avg[None, :],
+                                                                                    trend_filter_cutoff_freq,
+                                                                                    sampling_rate).squeeze()
+
+    raw_roi_lowpass = raw_roi_avg - masknmf.demixing.filters.high_pass_filter_batch(raw_roi_avg[None, :],
+                                                                                    trend_filter_cutoff_freq,
+                                                                                    sampling_rate).squeeze()
+
+    resid_roi_avg = raw_roi_avg - pmd_roi_avg
+
+    raw_roi_avg_noisevar = masknmf.diagnostics.voltage_diagnostics.onephoton_voltage_noise_heuristic(raw_roi_avg, 800,
+                                                                                                     300)
+    resid_std = np.std(resid_roi_avg)
+
+    ## Next step: let's take the C matrix and compute the spike locations (after running a 3Hz high pass filter)
+    hp_filter_c = masknmf.demixing.filters.high_pass_filter_batch(temporal_trace[None, :],
+                                                                  spike_detect_cutoff_freq,
+                                                                  sampling_rate).squeeze()
+
+    thres_c = masknmf.diagnostics.voltage_diagnostics.mad_filter_for_spikes(hp_filter_c, mad_cutoff)
+    c_peaks, _ = masknmf.diagnostics.voltage_diagnostics.detect_peaks(thres_c, distance=10)
+
+    # For each peak, estimate the splike amplitude. Strategy: subtract the 1Hz low-pass
+    pmd_spike_heights = pmd_roi_avg[c_peaks] - pmd_roi_lowpass[c_peaks]
+    raw_spike_heights = raw_roi_avg[c_peaks] - raw_roi_lowpass[c_peaks]
+
+    attenuation_estimate = raw_spike_heights - pmd_spike_heights
+    attenuation_estimate[attenuation_estimate < 0] = 0
+    fractional_attenuation = np.nan_to_num(np.abs(attenuation_estimate) / raw_spike_heights, nan=0.0)
+    zscore_attenuation = np.nan_to_num(np.abs(attenuation_estimate) / resid_std, nan=0.0)
+
+    return raw_roi_avg, pmd_roi_avg, c_peaks, pmd_spike_heights, raw_spike_heights, attenuation_estimate, fractional_attenuation, zscore_attenuation
+
+
