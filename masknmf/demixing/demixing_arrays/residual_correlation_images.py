4d8657c59210c9f82e6580ff17fb418231580134
apasarkar <amolpasarkar@gmail.com>
2025-10-23 10:54:33 +0800

    Includes further refactoring where the test spatial crop effect function is moved to a utils module

--- masknmf/demixing/demixing_arrays/residual_correlation_images.py
+++ masknmf/demixing/demixing_arrays/residual_correlation_images.py
@@ -3,75 +3,7 @@ from typing import *
 import numpy as np
 from masknmf.arrays.array_interfaces import FactorizedVideo
 import torch
-
-def test_slice_effect(my_slice: slice, spatial_dim: int) -> bool:
-    """
-    Returns True if slice will actually have an effect
-    """
-
-    if not (
-        (isinstance(my_slice.start, int) and my_slice.start == 0)
-        or my_slice.start is None
-    ):
-        return True
-    elif not (
-        (isinstance(my_slice.stop, int) and my_slice.stop >= spatial_dim)
-        or my_slice.stop is None
-    ):
-        return True
-    elif not (
-        my_slice.step is None or (isinstance(my_slice.step, int) and my_slice.step == 1)
-    ):
-        return True
-    return False
-
-
-def test_range_effect(my_range: range, spatial_dim: int) -> bool:
-    """
-    Returns True if the range will actually have an effect.
-
-    Parameters:
-    my_range (range): The range object to test.
-    spatial_dim (int): The size of the dimension that the range is applied to.
-
-    Returns:
-    bool: True if the range will affect the selection; False otherwise.
-    """
-    # Check if the range starts from the beginning
-    if my_range.start != 0:
-        return True
-    # Check if the range stops at the end of the dimension
-    elif my_range.stop != spatial_dim:
-        return True
-    # Check if the range step is not 1
-    elif my_range.step != 1:
-        return True
-    return False
-
-
-def test_spatial_crop_effect(my_tuple, spatial_dims) -> bool:
-    """
-    Returns true if the tuple used for spatial cropping actually has an effect on the underlying data. Otherwise
-    cropping can be an expensive and avoidable operation.
-    """
-    for k in range(len(my_tuple)):
-        if isinstance(my_tuple[k], np.ndarray):
-            if my_tuple[k].shape[0] < spatial_dims[k]:
-                return True
-
-        if isinstance(my_tuple[k], np.integer):
-            return True
-
-        if isinstance(my_tuple[k], int):
-            return True
-
-        if isinstance(my_tuple[k], slice):
-            if test_slice_effect(my_tuple[k], spatial_dims[k]):
-                return True
-        if isinstance(my_tuple[k], range):
-            if test_range_effect(my_tuple[k], spatial_dims[k]):
-                return True
-    return False
+from masknmf.demixing.demixing_arrays.demixing_array_utils import test_spatial_crop_effect
 
 class ResidCorrMode(Enum):
     DEFAULT = 0
