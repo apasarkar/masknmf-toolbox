d9e81c60bca11364bbb022e45b78a16fae2822ac
apasarkar <amolpasarkar@gmail.com>
2025-10-27 12:10:27 +0800

    Fixes tests to reflect new api

--- tests/test_masknmf.py
+++ tests/test_masknmf.py
@@ -10,17 +10,13 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 def test_rigid_motion_gpu():
     x = (np.random.rand(4, 32, 32) * 4096).astype(np.int16)
     rigid = masknmf.RigidMotionCorrector(max_shifts=(3, 3))
-    rigid = masknmf.motion_correction.compute_template(
-        x, rigid, num_iterations_piecewise_rigid=0, device=DEVICE
-    )
+    rigid.compute_template(x, device=DEVICE)
     out = masknmf.RegistrationArray(x, rigid, device='cuda')[:]
     assert out.shape == x.shape
 
 def test_rigid_motion_cpu():
     x = (np.random.rand(4, 32, 32) * 4096).astype(np.int16)
     rigid = masknmf.RigidMotionCorrector(max_shifts=(3, 3))
-    rigid = masknmf.motion_correction.compute_template(
-        x, rigid, num_iterations_piecewise_rigid=0, device='cpu'
-    )
+    rigid.compute_template(x, device=DEVICE)
     out = masknmf.RegistrationArray(x, rigid, device='cpu')[:]
     assert out.shape == x.shape
