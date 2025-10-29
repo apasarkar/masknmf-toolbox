1029538a62a28a5a9bb965f758a63afec350ceb2
apasarkar <amolpasarkar@gmail.com>
2025-10-30 03:52:05 +0800

    Updates tests for moco to reflect new api

--- tests/test_masknmf.py
+++ tests/test_masknmf.py
@@ -3,20 +3,17 @@ import torch
 import masknmf
 import pytest
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU unavailable")
 def test_rigid_motion_gpu():
     x = (np.random.rand(4, 32, 32) * 4096).astype(np.int16)
     rigid = masknmf.RigidMotionCorrector(max_shifts=(3, 3))
-    rigid.compute_template(x, device=DEVICE)
-    out = masknmf.RegistrationArray(x, rigid, device='cuda')[:]
+    rigid.compute_template(x)
+    out = masknmf.RegistrationArray(x, rigid)[:]
     assert out.shape == x.shape
 
 def test_rigid_motion_cpu():
     x = (np.random.rand(4, 32, 32) * 4096).astype(np.int16)
     rigid = masknmf.RigidMotionCorrector(max_shifts=(3, 3))
-    rigid.compute_template(x, device=DEVICE)
-    out = masknmf.RegistrationArray(x, rigid, device='cpu')[:]
+    rigid.compute_template(x)
+    out = masknmf.RegistrationArray(x, rigid)[:]
     assert out.shape == x.shape
