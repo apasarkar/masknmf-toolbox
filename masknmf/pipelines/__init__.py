747cd303344f2016fc31318f257ea5271aa442ba
apasarkar <amolpasarkar@gmail.com>
2026-03-20 23:06:44 +0800

    Includes singlechannel widefield pipeline code

--- masknmf/pipelines/__init__.py
+++ masknmf/pipelines/__init__.py
@@ -1,8 +1,8 @@
-from masknmf.pipelines.widefield.widefield_calcium import widefield_singlechannel_pipeline
+from masknmf.pipelines.widefield.widefield_calcium import WidefieldSinglechannelPipeline
 from masknmf.pipelines.twophoton_calcium.twophoton_population_imaging import TwoPhotonCalciumPipeline
 from masknmf.pipelines.configs import *
 
 __all__ = [
     "TwoPhotonCalciumPipeline",
-    "widefield_singlechannel_pipeline",
+    "WidefieldSinglechannelPipeline",
 ]
