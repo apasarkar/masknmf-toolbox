721a0177368053777c036b7a1e0ec9a1dc3f6658
Flynn OConnell <flynnoconnell@gmail.com>
2026-08-28 12:17:37 -0500

    export the shared widget surface

--- masknmf/visualization/imgui/__init__.py
+++ masknmf/visualization/imgui/__init__.py
@@ -5,12 +5,63 @@ from masknmf.visualization.imgui.picking import (
     zoom_to_bbox,
 )
 from masknmf.visualization.imgui.layout import resolve_time_reference, is_notebook_canvas
+from masknmf.visualization.imgui.movie_player import MoviePlayer
+from masknmf.visualization.imgui.labels import (
+    LABEL_COLORS,
+    LABEL_KEYS,
+    UNLABELED,
+    LabelSet,
+)
+from masknmf.visualization.imgui.table import (
+    FILTER_ALL,
+    RoiOrder,
+    draw_filter_row,
+    draw_roi_table,
+)
+from masknmf.visualization.imgui.overlay import (
+    OverlayPair,
+    footprint_rgba,
+    label_image_rgba,
+)
+from masknmf.visualization.imgui.masks import LabelImage
+from masknmf.visualization.imgui.draw import StrokeDrawer
+from masknmf.visualization.imgui.crop import context_crop, crop_origin
+from masknmf.visualization.imgui.panels import (
+    draw_keybinds_popup,
+    draw_label_buttons,
+    draw_label_editor,
+    draw_progress,
+)
+from masknmf.visualization.imgui.store import LabelStore
+from masknmf.visualization.imgui.loader import AsyncLoad
 
 __all__ = [
+    "AsyncLoad",
     "CheckboxWindow",
+    "FILTER_ALL",
+    "LABEL_COLORS",
+    "LABEL_KEYS",
+    "LabelImage",
+    "LabelSet",
+    "LabelStore",
+    "MoviePlayer",
+    "OverlayPair",
+    "RoiOrder",
+    "StrokeDrawer",
+    "UNLABELED",
     "component_at_pixel",
+    "context_crop",
     "contours_to_bbox",
-    "zoom_to_bbox",
-    "resolve_time_reference",
+    "crop_origin",
+    "draw_filter_row",
+    "draw_keybinds_popup",
+    "draw_label_buttons",
+    "draw_label_editor",
+    "draw_progress",
+    "draw_roi_table",
+    "footprint_rgba",
     "is_notebook_canvas",
+    "label_image_rgba",
+    "resolve_time_reference",
+    "zoom_to_bbox",
 ]
