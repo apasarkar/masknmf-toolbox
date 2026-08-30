1fdcb14ecf49a351269374894e122f97211a9a13
Flynn OConnell <FlynnOConnell@gmail.com>
2026-08-29 20:34:25 -0500

    add shared imgui theme: card, section, popup helpers styled after imgui_data_loader

--- masknmf/visualization/imgui/__init__.py
+++ masknmf/visualization/imgui/__init__.py
@@ -5,9 +5,17 @@ from masknmf.visualization.imgui.picking import (
     zoom_to_bbox,
 )
 from masknmf.visualization.imgui.layout import resolve_time_reference, is_notebook_canvas
+from masknmf.visualization.imgui.theme import Theme, THEME, to_vec4, em, card, section, popup
 
 __all__ = [
     "CheckboxWindow",
+    "Theme",
+    "THEME",
+    "to_vec4",
+    "em",
+    "card",
+    "section",
+    "popup",
     "component_at_pixel",
     "contours_to_bbox",
     "zoom_to_bbox",
