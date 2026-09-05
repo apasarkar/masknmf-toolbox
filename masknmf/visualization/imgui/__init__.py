b2f3ac65835eaea6faf6e62700a3fc1ea8ae9839
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2026-09-04 22:27:04 -0500

    Switches fpl traces for imgui traces  (#240)
    * init stacked lineplot
    
    * init compression vis with imgui traces

--- masknmf/visualization/imgui/__init__.py
+++ masknmf/visualization/imgui/__init__.py
@@ -4,11 +4,20 @@ from masknmf.visualization.imgui.picking import (
     contours_to_bbox,
     zoom_to_bbox,
 )
-from masknmf.visualization.imgui.layout import resolve_time_reference, is_notebook_canvas
+from masknmf.visualization.imgui.layout import (
+    HANDLE_THICKNESS,
+    draw_edge_handle,
+    is_notebook_canvas,
+    resolve_time_reference,
+)
+from masknmf.visualization.imgui.trace_plot import TracePlot
 from masknmf.visualization.imgui.theme import Theme, THEME, to_vec4, em, card, section, popup
 
 __all__ = [
     "CheckboxWindow",
+    "HANDLE_THICKNESS",
+    "TracePlot",
+    "draw_edge_handle",
     "Theme",
     "THEME",
     "to_vec4",
