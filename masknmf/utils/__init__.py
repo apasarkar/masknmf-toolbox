a1437896c58d5eba334e713b93451bb746b3cbf5
apasarkar <amolpasarkar@gmail.com>
2026-09-22 06:57:50 +0800

    Includes tensor types module so we can specify torch sparse tensors

--- masknmf/utils/__init__.py
+++ masknmf/utils/__init__.py
@@ -1,9 +1,12 @@
 from .display import display, get_timestamp
 from ._serialization import Serializer, has_group, drop_group
 from ._cuda import torch_select_device
+from .tensor_types import SparseCOOTensor, SparseCSRTensor
 
 __all__ = ["display",
            "get_timestamp",
            "Serializer",
            "has_group",
-           "drop_group"]
+           "drop_group",
+           "SparseCSRTensor",
+           "SparseCOOTensor"]
