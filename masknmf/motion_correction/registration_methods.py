8bc58ce67acac4d5bcdfbcaf4a6f84a76ba33efa
apasarkar <amolpasarkar@gmail.com>
2026-09-23 23:47:31 +0800

    Updates the demo notebooks

--- masknmf/motion_correction/registration_methods.py
+++ masknmf/motion_correction/registration_methods.py
@@ -383,7 +383,7 @@ def compute_stride_routine(shape: tuple[int, int, int],
     Args:
         shape (tuple[int, int, int]): Describes shape of imaging data (num_frames, fov_height, fov_width)
         minimum_patch_sizes (tuple[int, int]): The minimum (height, width) patch dimensions used to compute local
-            rigid shifts in the piecewise rigid shifts
+            rigid shifts in the piecewise rigid registration method
         overlaps (tuple[int, int]): The amount of overlap in each dimension between adjacent blocks
     Returns:
         tuple[tuple[int, int], torch.Tensor, torch.Tensor]: A tuple describing the (a) strides in both dimensions and the start points for
