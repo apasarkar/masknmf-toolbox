c7de4bf267701c0ecc7c87c877be0a46ac9d5123
apasarkar <amolpasarkar@gmail.com>
2026-09-23 08:41:14 +0800

    Renames file and updates init

--- masknmf/demixing/demixing_arrays/__init__.py
+++ masknmf/demixing/demixing_arrays/__init__.py
@@ -3,7 +3,7 @@ from masknmf.demixing.demixing_arrays.standard_correlation_images import Standar
 from masknmf.demixing.demixing_arrays.residual_correlation_images import ResidualCorrelationImages, ResidCorrMode
 from masknmf.demixing.demixing_arrays.fluctuating_background_array import FluctuatingBackgroundArray
 from masknmf.demixing.demixing_arrays.static_baseline import StaticBackgroundArray
-from masknmf.demixing.demixing_arrays.colorful_ac_array import ColorfulSignalsArray
+from masknmf.demixing.demixing_arrays.colorful_signals_array import ColorfulSignalsArray
 from masknmf.demixing.demixing_arrays.residual_array import ResidualArray
 from masknmf.demixing.demixing_arrays.multiunit_background_array import MultiunitBackgroundArray
 
